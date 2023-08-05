be06b50d4c7651f073225d7270b5c8f1318e5f75
hafsa mire <hafsamire80@gmail.com>
2023-08-05 16:46:08 +0100

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Heropage from './components/Heropage';
+import Experience from './components/Experience';
+
+function App() {
+  return (
+    <div className="App">
+        <Heropage />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Experience.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Experience() {
+  return (
+    <div>
+        <div class="container">
+            <div class-="">
+                <h1>Junior dev</h1>
+                <h3>Junior dev</h3>
+                <p>Developed key branding and promotion features, and increase seasonal product sales for encouraging 1.5M site users, maintaining the client's branding guidelines.
+Design internal front-end applications from planning to execution, using React.js, Tailwind.js and express.js, to create responsive web applications with various screen sizes
+Implemented and introduced call-to-action (CTA) button features, resulting in a 68% increase in user bookings.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Experience

0b2d210561d4ef0389f55f80172f5175762a5ff6
hafsa mire <hafsamire80@gmail.com>
2023-08-23 16:56:30 +0100

    - expreience added

--- my-app/src/components/Experience.js
+++ my-app/src/components/Experience.js
@@ -1,63 +1,82 @@
 import React from 'react'
+import greeneking from "../Asset/greeneking.jpeg"
 
 function Experience() {
   return (
     <div className="mx-24">
         <div class="container px-6 ">
+        <h1 class="font-bold text-lg text-left pb-5">Experience</h1>
+        <div class="px-20 py-4">
+    <h1 class="font-semibold text-lg text-left text-[#28D2B4]">About me</h1>
 
-            <div class="mx-20">
-              <h1 class="font-bold text-lg text-left pb-5">Experience</h1>
-                <h1 class="font-regular text-lg text-left text-[#A3F0FF]">Zone Digital</h1>
-                <h1 class="text-md text-left text-slate-400 pb-6">Junior front-end developer</h1>
-                <ul class="list-none text-left pb-4 text-slate-200 text-sm">
+    <ul class="list-none text-left pb-4 text-slate-300 text-sm hover:text-slate-100">
+<li class="pt-2">My coding journey began in 2021 when I started creating a static website from Dribble Designs. I was inspired to learn web development and enrolled in a tech accelerator boot camp where I learned the fundamentals of web development. Fast forward to today, I've had the privilege to work with the brilliant team at Zone Digital. Here, I created an internal Zone Car Park application, collaborated on and maintained the Greene King platform.</li>
+<li class="pt-2">My goal is to create websites that not only look great but also deliver a seamless user experience. I am a creative problem solver and enjoy tackling challenges that come with web development. I am committed to constantly improving my skills and staying up-to-date with the latest technologies.</li>
+
+<li class="pt-2">In my free time, I enjoy horse-riding and bouldering. These activities provide me with a fresh perspective and often inspire me in my work.
+I am always looking for new opportunities to work on exciting projects and collaborate with other professionals in the industry. If you are looking for a talented and dedicated front-end developer, please  contact me</li>
+
+</ul>
+
+
+</div>
+            <div class="px-20 py-4 h-full w-full hover:bg-[#010E11] rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 ">
+           
+
+                <h1 class="font-semibold text-sm text-left text-[#8EFCE1]">Zone Digital</h1>
+                <h1 class="text-lg text-left text-slate-100 pb-4">Junior front-end developer</h1>
+                <h1 class="text-sm  text-left text-slate-100  pb-4">10/2021 - 02/2023</h1>
+                <ul class="list-none text-left pb-4 text-slate-300 text-sm hover:text-slate-100">
   <li>Developed key branding and promotion features, and increase seasonal product sales for encouraging 1.5M site users, maintaining the client's branding guidelines.</li>
 </ul>
 
-<ol class="list-none text-left pb-4 text-slate-200 text-sm">
+<ul class="list-none text-left pb-4 text-slate-300 text-sm hover:text-slate-100">
   <li>Design internal front-end applications from planning to execution, using React.js, Tailwind.js and express.js, to create responsive web applications with various screen sizes</li>
-</ol>
+</ul>
 
-<ul class="list-none text-left pb-4 text-slate-200 text-sm" >
+<ul class="list-none text-left pb-4 text-slate-300 text-sm hover:text-slate-100" >
   <li>Implemented and introduced call-to-action (CTA) button features, resulting in a 68% increase in user bookings.</li>
 </ul>
             </div>
         </div>
         <div class="container px-6">
 
-<div class="mx-20">
+<div class="px-20 py-4 h-full w-full hover:bg-[#010E11] rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 ">
 
-    <h1 class="font-regular text-lg text-left text-[#A3F0FF]">Zone Digital</h1>
-    <h1 class="text-md text-left text-slate-400  pb-6">Junior front-end developer</h1>
-    <ul class="list-none text-left pb-4 text-slate-200 text-sm">
+    <h1 class="font-semibold text-sm text-left text-[#8EFCE1]">Tech accelerator</h1>
+    <h1 class="text-lg text-left text-slate-100  pb-4">Junior front-end developer</h1>
+    <h1 class="text-sm text-left text-slate-100  pb-4">10/2021 - 02/2023</h1>
+    <ul class="list-none text-left pb-4 text-slate-300 text-sm hover:text-slate-100">
 <li>Developed key branding and promotion features, and increase seasonal product sales for encouraging 1.5M site users, maintaining the client's branding guidelines.</li>
 </ul>
 
-<ol class="list-none text-left pb-4 text-slate-200 text-sm">
+<ol class="list-none text-left pb-4 text-slate-300 text-sm hover:text-slate-100">
 <li>Design internal front-end applications from planning to execution, using React.js, Tailwind.js and express.js, to create responsive web applications with various screen sizes</li>
 </ol>
 
-<ul class="list-none text-left pb-4 text-slate-200 text-sm">
+<ul class="list-none text-left pb-4 text-slate-300 text-sm hover:text-slate-100">
 <li>Implemented and introduced call-to-action (CTA) button features, resulting in a 68% increase in user bookings.</li>
 </ul>
 </div>
 </div>
-<div class="container px-6">
+<div class="container px-6 ">
 
-<div class="mx-20">
-    <h1 class="font-regular text-lg text-left text-[#A3F0FF]">Zone Digital</h1>
-    <h1 class="text-md text-left text-slate-400  pb-6">Junior front-end developer</h1>
-    <ul class="list-none text-left pb-4 text-slate-200 text-sm">
-<li>Developed key branding and promotion features, and increase seasonal product sales for encouraging 1.5M site users, maintaining the client's branding guidelines.</li>
-</ul>
-
-<ol class="list-none text-left pb-4 text-slate-200 text-sm">
-<li>Design internal front-end applications from planning to execution, using React.js, Tailwind.js and express.js, to create responsive web applications with various screen sizes</li>
-</ol>
-
-<ul class="list-none text-left pb-4 text-slate-200 text-sm">
-<li>Implemented and introduced call-to-action (CTA) button features, resulting in a 68% increase in user bookings.</li>
-</ul>
+<div class="px-20 py-4 h-full w-full bg-[#010E11] rounded-md  backdrop-filter backdrop-blur-lg bg-opacity-30 ">
+           
+<div class="flex flex-row">
+  <div class="basis-3/4 p-4">   
+   <h1 class="font-semibold text-xs text-left text-[#8EFCE1] pb-2">Web development</h1>
+           <h1 class="text-lg text-left text-slate-100 pb-2">Greene King</h1>
+           {/* <h1 class="text-sm  pt-2 text-left text-slate-100  pb-4"></h1> */}
+           <p  class="text-sm  pt-2 text-left text-slate-100  pb-4">Developed key branding and promotion features, and increase seasonal product sales for encouraging 1.5M site users, maintaining the client's branding guidelines.</p>
+         
 </div>
+<div class="basis-1/4">
+<img src={greeneking} alt="React Image" />
+</div>
+</div>
+        
+       </div>
 </div>
     </div>
   )
--- my-app/src/components/Heropage.js
+++ my-app/src/components/Heropage.js
@@ -112,32 +112,29 @@ export default function Heropage() {
             </div>
           </div> */}
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-[#A3F0FF] sm:text-6xl">
+            <h1 className="text-4xl font-bold tracking-tight text-[#28D2B4] sm:text-6xl">
             Hafsa Mire
             </h1>
-            <p className="mt-6 text-lg leading-8 text-slate-300">
+            <p className="mt-6 text-md leading-8 text-slate-300">
             I am Hafsa Mire, an experienced front-end developer and UX designer passionate about creating engaging and intuitive user experiences. 
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md  px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                Contact me
+               <span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+</svg></span>
+
               </a>
-              {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
-              </a> */}
+
+             
             </div>
           </div>
         </div>
-        {/* <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-  
-          /> */}
+     
         </div>
       </div>
 
